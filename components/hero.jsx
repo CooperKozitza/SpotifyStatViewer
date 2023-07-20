1134d691395d90adde2d5f55f9011743847de7d8
CooperKozitza <95152307+CooperKozitza@users.noreply.github.com>
2023-07-19 17:56:34 -0700

    Fixed Spacing, Added Button

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import LoginButton from "./login";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 
@@ -19,22 +19,21 @@ const HeroSection = () => {
             top: '0',
             left: '0',
             marginTop: '50px',
-        }} className="d-flex flex-column justify-content-end align-items-start">
-            <Row className="align mx-auto" style={{maxWidth: "960px"}}> 
-                <Col md={7} className="p-4 text-light bg-dark rounded">
+        }} className="d-flex p-0 flex-column justify-content-end align-items-start">
+            <Row className="align mx-auto px-2" style={{maxWidth: "80vw", marginBottom: '20px'}}> 
+                <Col lg={7} className="p-4 text-light bg-dark rounded">
                     <h1>Unveil Your Music Taste</h1>
                     <p>Connect with Spotify and explore your listening statistics, top tracks, artists, albums, and get tailored recommendations.</p>
                     <span className="d-flex justify-content-center justify-content-sm-start">
                         { !loggedIn 
                             ? <LoginButton>Login With Spotify</LoginButton>
-                            : <a onClick={() => router.push('/dashboard')}>Go to your dashboard <FontAwesomeIcon icon={faCaretRight} /></a>
+                            : <Button 
+                                variant="outline-light" 
+                                onClick={() => router.push('/dashboard')}>
+                                    Go to your dashboard <FontAwesomeIcon icon={faCaretRight} />
+                            </Button>
                         }
                     </span>
-                </Col>
-            </Row>
-            <Row className="align mx-auto" style={{maxWidth: "960px"}}> 
-                <Col className="my-2">
-                    <FontAwesomeIcon icon={faCaretDown} />
                 </Col>
             </Row>
         </Container>
